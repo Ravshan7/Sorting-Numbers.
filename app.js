69372e0944b02fe9e8ae494d081e5275fa9e97af
Kendjaev Ravshan <rjannn02@gmail.com>
2021-05-01 18:29:53 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+
+function random() {
+    return Math.round(Math.random() * 100)
+}
+
+function min(arr, idx) {
+    debugger
+    let min = arr[idx]
+    let index = idx
+    for (let i = idx + 1; i < arr.length; i++) {
+        if (min > arr[i]) {
+            min = arr[i]
+            index = i
+        }
+    }
+    return index
+}
+
+function swap(arr, i, min) {
+    debugger
+    let c = arr[min] 
+    arr[min] = arr[i]
+    arr[i] = c
+}
+
+
+const array = new Array(10).fill('').map(() => random())
+console.log(array);
+
+
+for (let i = 0; i < array.length; i++) {
+    debugger
+    let minArr = min(array, i)
+    swap(array, i, minArr)
+}
+
+console.log(array);
+
